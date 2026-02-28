a40b75469b4bc29e2e121bc737fde240deee8fdf
alchemistlowkey <alchemistlowkey@gmail.com>
2026-02-28 22:30:30 +0100

    modified for cli

--- ExpenseTrackerCli/obj/Release/net10.0/ExpenseTrackerCli.AssemblyInfo.cs
+++ ExpenseTrackerCli/obj/Release/net10.0/ExpenseTrackerCli.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTrackerCli")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a2a186bd6e355624e4e279214a2e4cec42c6e55")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTrackerCli")]
